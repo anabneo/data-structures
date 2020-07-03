254a1ad1f09118c1a00a05b12caf456315ff1c36
“Ana <ana.neo@mercadolivre.com>
2020-07-03 19:11:42 -0300

    storage sequence

--- src/main/java/Vetor.java
+++ src/main/java/Vetor.java
@@ -6,31 +6,76 @@ public class Vetor {
     private int totalDeAlunos = 0;
 
     public void adicionaAluno(Aluno aluno) {
-        for(int i = 0; i < alunos.length; i++){
-            if (alunos[i] == null){
-                alunos[i] = aluno;
-                break;
+        this.garanteEspaco();
+        this.alunos[totalDeAlunos] = aluno;
+        totalDeAlunos++;
+    }
+
+    private boolean posicaoValida(int posicao) {
+        return posicao >= 0 && posicao <= totalDeAlunos;
+    }
+
+    private void garanteEspaco() {
+        if (totalDeAlunos == alunos.length) {
+            Aluno[] novoArray = new Aluno[alunos.length * 2];
+            for (int i = 0; i < alunos.length; i++) {
+                novoArray[i] = alunos[i];
             }
+
+            this.alunos = novoArray;
         }
+
+    }
+
+    public void adiciona(int posicao, Aluno aluno) {
+
+        this.garanteEspaco();
+
+        if (!posicaoValida(posicao)) {
+            throw new IllegalArgumentException("posição inválida");
+        }
+
+        for (int i = totalDeAlunos - 1; i >= posicao; i -= 1) {
+            alunos[i + 1] = alunos[i];
+        }
+
+        alunos[posicao] = aluno;
+        totalDeAlunos++;
+    }
+
+    private boolean posicaoOcupada(int posicao) {
+        return posicao >= 0 && posicao < totalDeAlunos;
     }
 
     public Aluno pego(int posicao) {
 
-        return null;
+        if (!posicaoOcupada(posicao)) {
+            throw new IllegalArgumentException("posição inválida");
+        }
+        return alunos[posicao];
     }
 
     public void remove(int posicao) {
-
+        for (int i = posicao; i < this.totalDeAlunos; i++) {
+            this.alunos[i] = this.alunos[i + 1];
+        }
+        totalDeAlunos--;
     }
 
     public boolean contem(Aluno aluno) {
+
+        for (int i = 0; i < totalDeAlunos; i++) {
+            if (aluno.equals(alunos[i])) {
+                return true;
+            }
+        }
 
         return false;
     }
 
     public int tamanho() {
 
-        return 0;
+        return totalDeAlunos;
     }
 
     public String toString() {
--- src/main/java/VetorTest.java
+++ src/main/java/VetorTest.java
@@ -6,9 +6,31 @@ public class VetorTest {
 
         Vetor lista = new Vetor();
 
+        System.out.println(lista.tamanho());
         lista.adicionaAluno(a1);
+        System.out.println(lista.tamanho());
         lista.adicionaAluno(a2);
+        System.out.println(lista.tamanho());
 
+        System.out.println(lista);
+        System.out.println(lista.contem(a1));
+
+        Aluno a3 = new Aluno("Danilo");
+        System.out.println(lista.contem(a3));
+
+        Aluno x = lista.pego(1);
+        System.out.println(x);
+
+        lista.adiciona(1, a3);
+        System.out.println(lista);
+
+        lista.remove(1);
+        System.out.println(lista);
+
+        for(int i = 0; i < 300; i++){
+            Aluno y = new Aluno("Joao " + i);
+            lista.adicionaAluno(y);
+        }
         System.out.println(lista);
     }
 }
